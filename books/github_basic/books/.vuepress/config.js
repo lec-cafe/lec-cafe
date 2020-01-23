6ec914e0f7af57d71dd3838bcdba5e189d79dcb2
mikakane <t.goto@chatbox-inc.com>
2020-01-23 20:57:13 +0900

    updte

--- books/github_basic/books/.vuepress/config.js
+++ books/github_basic/books/.vuepress/config.js
@@ -1,7 +1,10 @@
+const commonConfig = require("../../../../libs/common.config")
+
 // .vuepress/config.js
 module.exports = {
-    title: 'Terminal での Git/Github入門 - Netlify でWebサイト公開',
-    description: `
+  ...commonConfig,
+  title: 'Terminal での Git/Github入門 - Netlify でWebサイト公開',
+  description: `
 Terminal (黒い画面) を使っての Git/Github の使い方をマスターします。
 
 Githubを操作しながら Web サイトの公開ができる Netlify を利用して、 実際のWebサイト制作をイメージしながら Git の操作を体験してみましょう。
@@ -9,63 +12,39 @@ Githubを操作しながら Web サイトの公開ができる Netlify を利用
 Git の基本操作 (コミット・ブランチの操作)
 Github の使い方 （push / pull）
 GIthub のチームでの使い方 ( Issue / Pull Request )    `,
-    head: [
-        ['script', { src: "https://static.codepen.io/assets/embed/ei.js"}]
+  themeConfig: {
+    ...commonConfig.themeConfig,
+    nav: [
+      {text: 'Lec Café', link: 'https://leccafe.connpass.com/'},
     ],
-    locales: {
-        '/': {
-            lang: 'ja',
-        },
-    },
-    markdown: {
-        anchor: {
-            level: [1,2,3],
-            slugify: (s) => encodeURIComponent(String(s).trim().toLowerCase().replace(/\s+/g, '-')),
-            permalink: true,
-            permalinkBefore: true,
-            permalinkSymbol: '#'
-        },
-        config: md => {
-            md.use(require('markdown-it-playground'))
-        },
-        linkify: true
-    },
-    themeConfig: {
-        nav: [
-            { text: 'Lec Café', link: 'https://leccafe.connpass.com/' },
-        ],
-        sidebar: [
-            '/',
-            '/0.Gitの環境構築/',
-            '/1_terminal/',
-            '/2_start_github/',
-            '/3.branch/',
-            '/4.github_workflow/',
-            {
-                title: 'いろいろなGitコマンド',
-                children: [
-                    '/8_git_status/',
-                    '/8_git_remote/',
-                    '/8_git_push/',
-                    '/8_git_remote/',
-                    '/8_git_status/',
-                ]
-            },
-            {
-                title: 'GitHub の活用',
-                children: [
-                    '/9_netlify/',
-                    '/9_github_issues/',
-                    '/9_github_pullrequest/',
-                    '/9_github_flow/',
-                    '/9_github_actions/'
-                ]
-            },
-        ],
-        repo: 'lec-cafe/book_github_intro',
-        repoLabel: 'Github',
-        docsDir: 'books',
-        editLinks: true,
-        editLinkText: 'ページに不明点や誤字等があれば、Github にて修正を提案してください！'
-    }
+    sidebar: [
+      '/',
+      '/0.Gitの環境構築/',
+      '/1_terminal/',
+      '/2_start_github/',
+      '/3.branch/',
+      '/4.github_workflow/',
+      {
+        title: 'いろいろなGitコマンド',
+        children: [
+          '/8_git_status/',
+          '/8_git_remote/',
+          '/8_git_push/',
+          '/8_git_remote/',
+          '/8_git_status/',
+        ]
+      },
+      {
+        title: 'GitHub の活用',
+        children: [
+          '/9_netlify/',
+          '/9_github_issues/',
+          '/9_github_pullrequest/',
+          '/9_github_flow/',
+          '/9_github_actions/'
+        ]
+      },
+    ],
+    docsDir: 'books/github_basic/books',
+  }
 }
