c09eeb04b6d0875e698249f73a148ce4753d33f0
mikakane <t.goto@chatbox-inc.com>
2020-03-06 16:36:03 +0900

    update

--- books/jslang/books/.vuepress/config.js
+++ books/jslang/books/.vuepress/config.js
@@ -21,15 +21,20 @@ GIthub のチームでの使い方 ( Issue / Pull Request )    `,
       '/',
       '/1.JavaScriptと言語仕様',
       '/2.JavaScriptの基礎文法',
-      '/3.JavaScriptの文字列操作',
-      '/4.JavaScriptの配列操作',
-      '/5.JavaScriptのオブジェクト操作',
-      '/8. 関数の利用Untitled',
-      '/8.JavaScript文法の注意点',
       {
-        title: 'GitHub の活用',
+        title: 'JavaScriptの組み込み型',
         children: [
-          '/9.1.モジュールシステムの利用',
+          '/3.1.JavaScriptの文字列操作',
+          '/3.2.JavaScriptの配列操作',
+          '/3.3.JavaScriptのオブジェクト操作',
+          // '/3.4.関数の利用Untitled',
+        ]
+      },
+      '/4.JavaScript文法の注意点',
+      {
+        title: '補足資料',
+        children: [
+          // '/9.1.モジュールシステムの利用',
           '/9.2.Promise async await',
         ]
       },
