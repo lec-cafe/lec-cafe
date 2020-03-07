caaf1abf8948e6b1eb957cad5838aea14860be0e
mikakane <t.goto@chatbox-inc.com>
2020-03-07 16:56:36 +0900

    update

--- books/jslang/books/.vuepress/config.js
+++ books/jslang/books/.vuepress/config.js
@@ -3,15 +3,8 @@ const commonConfig = require("../../../../libs/common.config")
 // .vuepress/config.js
 module.exports = {
   ...commonConfig,
-  title: 'Terminal での Git/Github入門 - Netlify でWebサイト公開',
-  description: `
-Terminal (黒い画面) を使っての Git/Github の使い方をマスターします。
-
-Githubを操作しながら Web サイトの公開ができる Netlify を利用して、 実際のWebサイト制作をイメージしながら Git の操作を体験してみましょう。
-
-Git の基本操作 (コミット・ブランチの操作)
-Github の使い方 （push / pull）
-GIthub のチームでの使い方 ( Issue / Pull Request )    `,
+  title: 'JavaScript 基礎文法講座',
+  description: `JavaScript 基礎文法講座`,
   themeConfig: {
     ...commonConfig.themeConfig,
     nav: [
