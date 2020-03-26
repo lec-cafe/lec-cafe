1bf36ae90f62e3eb370d32c1d5b410a5b469f244
mikakane <t.goto@chatbox-inc.com>
2020-03-27 03:30:14 +0900

    update

--- books/laravel_php/.vuepress/config.js
+++ books/laravel_php/.vuepress/config.js
@@ -27,7 +27,23 @@ module.exports = {
       {text: 'Lec Café', link: 'https://leccafe.connpass.com/'},
     ],
     sidebar: [
-      '/1.PHPと言語仕様'
+      '/1.PHPと言語仕様',
+      '/2.PHP基礎文法',
+      {
+        title: 'PHPの関数操作',
+        children: [
+          '/3.1.PHPの文字列操作',
+          '/3.2.PHPの配列操作',
+          '/3.3.PHPのファイル操作',
+        ]
+      },
+      {
+        title: 'PHPのクラス操作',
+        children: [
+          '/4.1.PHPのクラス活用',
+        ]
+      },
+
     ],
     repo: 'lec-cafe/books_laravelapi_basic',
     repoLabel: 'Github',
