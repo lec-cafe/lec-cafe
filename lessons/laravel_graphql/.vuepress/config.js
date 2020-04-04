6eb9fbad0f462da2d61b5cdad39cd4054515206e
mikakane <t.goto@chatbox-inc.com>
2020-04-04 18:34:10 +0900

    update

--- lessons/laravel_graphql/.vuepress/config.js
+++ lessons/laravel_graphql/.vuepress/config.js
@@ -2,8 +2,8 @@ const commonConfig = require("../../../libs/common.config")
 
 module.exports = {
   ...commonConfig,
-  // base: "/laravel_graphql",
-  // dest: "dist/laravel_graphql",
+  base: "/laravel_graphql",
+  dest: "dist/laravel_graphql",
   title: 'Laravel GraphQL 開発 入門',
   description: 'Laravel と lighthouse を利用して GraphQL を作成するための 入門講座です。',
   themeConfig: {
--- lessons/nuxtjs_website/.vuepress/config.js
+++ lessons/nuxtjs_website/.vuepress/config.js
@@ -2,20 +2,21 @@ const commonConfig = require("../../../libs/common.config")
 
 module.exports = {
   ...commonConfig,
-  // base: "/laravel_graphql",
-  // dest: "dist/laravel_graphql",
+  base: "/nuxtjs_website",
+  dest: "dist/nuxtjs_website",
   title: 'Laravel GraphQL 開発 入門',
   description: 'Laravel と lighthouse を利用して GraphQL を作成するための 入門講座です。',
   themeConfig: {
     ...commonConfig.themeConfig,
     sidebar: [
       '/01.setup',
-      '/02.database',
-      '/03.query',
-      '/04.mutations',
-      '/05.mutations2',
-      '/06.input',
-      '/07.validations',
+      '/02.page',
+      '/03.layout',
+      '/04.routing',
+      '/05.scss',
+      '/06.build',
+      '/07.assets',
+      '/08.meta',
     ],
     docsDir: 'lessons/laravel_graphql',
   }
