61a658afc2cadd8f1c79d6c4fb52de0ba651565b
mikakane <t.goto@chatbox-inc.com>
2020-04-05 01:21:51 +0900

    update

---
+++ lessons/top/.vuepress/config.js
@@ -0,0 +1,15 @@
+const commonConfig = require("../../../libs/common.config")
+
+module.exports = {
+  ...commonConfig,
+  base: "/",
+  dest: "dist",
+  title: 'Lec Cafe',
+  description: 'Lec Cafe は動画で学べるオンラインのWeb制作セミナーです。',
+  themeConfig: {
+    ...commonConfig.themeConfig,
+    sidebar: [
+    ],
+    docsDir: 'lessons/toppage',
+  }
+}
